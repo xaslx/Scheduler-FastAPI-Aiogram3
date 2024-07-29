f3b417b0b9f4c08726cb6aaded007acc88cc46a5
maksim <knyazev.maksim2852@gmail.com>
2024-07-29 21:50:57 +0400

    del logging

--- app/repository/notification_repo.py
+++ app/repository/notification_repo.py
@@ -12,16 +12,6 @@ class NotificationRepository(BaseRepository):
 
     @classmethod
     async def find_all_notif(cls, session: AsyncSession):
-        try:
-            query = select(cls.model).order_by(cls.model.created_at.desc()).limit(3)
-            res = await session.execute(query)
-            return res.scalars().all()
-        except (SQLAlchemyError, Exception) as e:
-            if isinstance(e, SQLAlchemyError):
-                msg = 'Database Exc: Не удалось найти уведомление.'
-            else:
-                msg = 'Unknown Exc: Не удалось найти уведомление.'
-            logger.error(msg)
-            return None
-        
-    
+        query = select(cls.model).order_by(cls.model.created_at.desc()).limit(3)
+        res = await session.execute(query)
+        return res.scalars().all()
