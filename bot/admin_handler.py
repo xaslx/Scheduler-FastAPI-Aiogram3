7a6963fefb83ce8a5cd1a5d13d6be4504db9529b
Maksim <knyazev.maksim2852@gmail.com>
2024-08-17 15:19:59 +0400

    add the ability to send notifications to telegram users

--- bot/admin_handler.py
+++ bot/admin_handler.py
@@ -1,16 +1,56 @@
 from aiogram import Router
-from aiogram.filters import Filter, Command, StateFilter
-from config import settings
+from aiogram.filters import Command, StateFilter
+from app.schemas.tg_schema import TelegramOut
+from bot.bot_service import BotService
 from aiogram.types import Message
 from aiogram.fsm.state import default_state
 from bot.admin_filter import AdminProtect
-
+from bot.state_for_admin import CreateNotification
+from aiogram.fsm.context import FSMContext
+from app.tasks.tasks import send_notifications_for_all_users_tg
 
 admin_router: Router = Router()
 
 
 @admin_router.message(StateFilter(default_state), AdminProtect(), Command('apanel'))
 async def admins_panel(message: Message):
-    await message.answer('Команды администраторов:\n/create_notifications  -  Отправить рассылку')
+    await message.answer('Команды администраторов:\n/create_notification  -  Отправить рассылку')
 
 
+@admin_router.message(StateFilter(default_state), AdminProtect(), Command(commands='cancel'))
+async def process_cancel_command(message: Message):
+    await message.answer(
+        text='Отменять нечего. Вы пока не создаете уведомление\n\n'
+        'Чтобы перейти к созданию уведомления - '
+        'отправьте команду /create_notification'
+    )
+
+
+@admin_router.message(~StateFilter(default_state), AdminProtect(),  Command(commands='cancel'))
+async def process_cancel_command_state(message: Message, state: FSMContext):
+    await message.answer(
+        text='Вы отменили создание уведомления\n\n'
+        'Чтобы снова перейти к созданию уведомления - '
+        'отправьте команду /create_notification'
+    )
+    await state.clear()
+
+
+@admin_router.message(StateFilter(default_state), AdminProtect(), Command('create_notification'))
+async def create_notification(message: Message, state: FSMContext):
+    await state.set_state(CreateNotification.description)
+    await message.answer('Напишите сообщение, его увидят все пользователи которые пользуются ботом\n/cancel - Для отмены создания уведомления')
+
+
+@admin_router.message(StateFilter(CreateNotification.description), AdminProtect())
+async def add_description(message: Message, state: FSMContext):
+    await state.update_data(description=message.text)
+    users: list[TelegramOut] = await BotService.find_all_users()
+    users_id: list[int] = [user.telegram_id for user in users]
+    await message.answer(
+        f'Уведомление создано и будет отправлено пользователям\n'
+        f'Количество пользователей которые получат уведомление:  {len(users)}'
+    )
+    text: str = await state.get_data()
+    await state.clear()
+    await send_notifications_for_all_users_tg(users_id=users_id, text=text['description'])
