1866ff9246ed43ed37b290aeda215407a83e744c
Maksim <knyazev.maksim2852@gmail.com>
2024-08-17 13:17:23 +0400

    update user_handlers.py

--- bot/user_handler.py
+++ bot/user_handler.py
@@ -3,11 +3,9 @@ from aiogram.filters import Command, CommandStart, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
 from aiogram.types import CallbackQuery, Message
-from fastapi import Depends
-from database import get_async_session, async_session_maker
-from sqlalchemy.ext.asyncio import AsyncSession
-from app.repository.notification_repo import NotificationRepository
-from app.repository.user_repo import UserRepository
+from bot.bot_service import BotService
+from app.schemas.tg_schema import TelegramOut
+
 
 user_router: Router = Router()
 
@@ -15,9 +13,11 @@ user_router: Router = Router()
 
 @user_router.message(CommandStart(), StateFilter(default_state))
 async def cmd_start(message: Message):
-    await message.answer('Привет, это Бот от сайта Scheduler')
-    await message.answer('Ваш ID, скопируйте его и вставьте на сайте')
+    await message.answer('Привет, это Бот от сайта Scheduler\nВаш ID, скопируйте его и вставьте на сайте')
     await message.answer(str(message.from_user.id))
+    user: TelegramOut = await BotService.find_user_by_tg_id(telegram_id=message.from_user.id)
+    if not user:
+        await BotService.add_new_user(telegram_id=message.from_user.id)
 
 
 
