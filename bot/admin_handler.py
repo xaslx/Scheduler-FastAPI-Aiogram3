08fa02b199e34cb0e9fe1f97364e40a0f9e33e9f
Maksim <knyazev.maksim2852@gmail.com>
2024-08-21 12:41:06 +0400

    update command

--- bot/admin_handler.py
+++ bot/admin_handler.py
@@ -17,7 +17,7 @@ async def admins_panel(message: Message):
     await message.answer('Команды администраторов:\n/create_notification  -  Отправить рассылку')
 
 
-@admin_router.message(StateFilter(default_state), AdminProtect(), Command(commands='cancel'))
+@admin_router.message(StateFilter(default_state), AdminProtect(), Command(commands='acancel'))
 async def process_cancel_command(message: Message):
     await message.answer(
         text='Отменять нечего. Вы пока не создаете уведомление\n\n'
@@ -26,7 +26,7 @@ async def process_cancel_command(message: Message):
     )
 
 
-@admin_router.message(~StateFilter(default_state), AdminProtect(),  Command(commands='cancel'))
+@admin_router.message(~StateFilter(default_state), AdminProtect(),  Command(commands='acancel'))
 async def process_cancel_command_state(message: Message, state: FSMContext):
     await message.answer(
         text='Вы отменили создание уведомления\n\n'
@@ -39,7 +39,7 @@ async def process_cancel_command_state(message: Message, state: FSMContext):
 @admin_router.message(StateFilter(default_state), AdminProtect(), Command('create_notification'))
 async def create_notification(message: Message, state: FSMContext):
     await state.set_state(CreateNotification.description)
-    await message.answer('Напишите сообщение, его увидят все пользователи которые пользуются ботом\n/cancel - Для отмены создания уведомления')
+    await message.answer('Напишите сообщение, его увидят все пользователи которые пользуются ботом\n/acancel - Для отмены создания уведомления')
 
 
 @admin_router.message(StateFilter(CreateNotification.description), AdminProtect())
