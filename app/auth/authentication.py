4db82b1fcc5b82f401725a2c16c96e562d96b8fa
Maksim <knyazev.maksim2852@gmail.com>
2024-08-30 21:44:01 +0400

    add new generate token for telegram

--- app/auth/authentication.py
+++ app/auth/authentication.py
@@ -11,3 +11,9 @@ async def generate_token(user: User):
     user_to_email = {"sub": str(user.id), "exp": expire}
     token = jwt.encode(user_to_email, settings.SECRET_KEY, settings.ALGORITHM)
     return token
+
+async def generate_token_connect_tg(user_tg_id: str):
+    expire = datetime.utcnow() + timedelta(minutes=5)
+    user = {"sub": user_tg_id, "exp": expire}
+    token = jwt.encode(user, settings.SECRET_KEY, settings.ALGORITHM)
+    return token
