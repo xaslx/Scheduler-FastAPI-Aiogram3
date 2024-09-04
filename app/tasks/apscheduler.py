9d03d07176f1c6632042fbac832ca93282bc903a
Maksim <knyazev.maksim2852@gmail.com>
2024-09-04 16:20:58 +0400

    add sqlite bd

--- app/tasks/apscheduler.py
+++ app/tasks/apscheduler.py
@@ -1,6 +1,13 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from app.utils.generate_time import moscow_tz
+from config import settings
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
+
+
+jobstores = {
+    'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
+}
 
 scheduler: AsyncIOScheduler = AsyncIOScheduler()
-scheduler.configure(timezone=moscow_tz)
+scheduler.configure(timezone=moscow_tz, jobstores=jobstores)
