8ef134f7ee7143c0ccc3f76f06c4535adb6701a2
Maksim <knyazev.maksim2852@gmail.com>
2024-09-04 10:26:05 +0400

    add moscow tz

--- app/tasks/apscheduler.py
+++ app/tasks/apscheduler.py
@@ -1,3 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from app.utils.generate_time import moscow_tz
+
 
 scheduler: AsyncIOScheduler = AsyncIOScheduler()
+scheduler.configure(timezone=moscow_tz)
