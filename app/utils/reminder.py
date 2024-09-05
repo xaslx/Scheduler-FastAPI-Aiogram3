ce95bdc66bcf658123f4779244c6aac3980daaf3
Maksim <knyazev.maksim2852@gmail.com>
2024-09-05 18:33:17 +0400

    update

--- app/utils/reminder.py
+++ app/utils/reminder.py
@@ -7,5 +7,5 @@ def reminder(time_: str, date: date) -> datetime:
     time_split: list[str] = time_.split(':')
 
     booking_datetime: datetime = datetime.combine(date=date, time=time(hour=int(time_split[0]), minute=int(time_split[1])))
-    reminder_time: datetime = booking_datetime - timedelta(hours=3)
+    reminder_time: datetime = booking_datetime - timedelta(hours=2)
     return reminder_time
