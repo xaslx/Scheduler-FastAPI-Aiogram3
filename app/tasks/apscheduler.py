98e6c3f036fead66d2a731d48818cf305ad0e7cc
Maksim <knyazev.maksim2852@gmail.com>
2024-09-12 09:14:36 +0400

    update jobstores

--- app/tasks/apscheduler.py
+++ app/tasks/apscheduler.py
@@ -2,9 +2,9 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from app.utils.generate_time import moscow_tz
 from config import settings
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.jobstores.redis import RedisJobStore
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
 
 
-jobstores: dict[str, RedisJobStore] = {'default': RedisJobStore(db=2, host=settings.REDIS_HOST, port=settings.REDIS_PORT)}
+jobstores: dict[str, SQLAlchemyJobStore] = {'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')}
 scheduler: AsyncIOScheduler = AsyncIOScheduler(timezone=moscow_tz, jobstores=jobstores)
